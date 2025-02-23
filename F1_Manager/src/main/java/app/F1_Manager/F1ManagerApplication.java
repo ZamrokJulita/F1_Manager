38b8e9a9660543d82c762fb90ba12c3b8718c182
julita.zamroczynska <julita.zamroczynska@icloud.com>
2025-02-23 12:56:05 +0100

    init

---
+++ F1_Manager/src/main/java/app/F1_Manager/F1ManagerApplication.java
@@ -0,0 +1,13 @@
+package app.F1_Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1ManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1ManagerApplication.class, args);
+	}
+
+}
